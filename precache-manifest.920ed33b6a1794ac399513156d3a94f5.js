5ded9507596480b1c5597d5785272dd93a51fc0c
Dan Octavian <danoctavian91@gmail.com>
2019-12-31 20:47:40 +0800

    deployment 1

---
+++ precache-manifest.920ed33b6a1794ac399513156d3a94f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51d484e4be82ac5c82a1ef30d18c1407",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5502c22a96ce6ae080f2",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "5e57706553f38d17e899",
+    "url": "/static/css/main.52ce79a7.chunk.css"
+  },
+  {
+    "revision": "5502c22a96ce6ae080f2",
+    "url": "/static/js/2.d18446f6.chunk.js"
+  },
+  {
+    "revision": "5e57706553f38d17e899",
+    "url": "/static/js/main.4966921a.chunk.js"
+  },
+  {
+    "revision": "543c1f1f442d17811d5b",
+    "url": "/static/js/runtime-main.b84a449c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
